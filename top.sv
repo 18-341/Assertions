54c2a555ccf8eeaae0ecdad85f253c2c5f2af7ba
Ford Seidel <flseidel@gmail.com>
2017-10-13 01:23:17 -0400

    Add sample test mentioned in writeup
    I accidentally removed it, oops.

--- top.sv
+++ top.sv
@@ -36,7 +36,6 @@ module top();
     logic         stackOverflow, unexpectedDone, protocolError, dataOverflow, 
                 correct, finished;
 
-    // your view of the stack.  Level i of the stack is stackOut[i]
     logic [7:0][15:0] stackOut;
   
     //calculator instantiation
@@ -54,6 +53,24 @@ module top();
         /* * * * * * * * * * * * 
          * YOUR TESTBENCH HERE *
          * * * * * * * * * * * */
+        //test for functional "correct", "finished", and ADD
+        @(posedge clock);
+        data.op <= START;
+        data.payload <= 16'h0005; // stack: 5
+        @(posedge clock);
+        data.op <= ENTER;
+        data.payload <= 16'h0003; // stack: 3 5
+        @(posedge clock);
+        data.op <= ARITH_OP;
+        data.payload <= 16'h1; // add
+        @(posedge clock);
+        data.op <= DONE;
+        data.payload <= 16'h1; // done
+        @(posedge clock);
+        assert(correct);
+        assert(finished);
+        assert(result == 8)
+          else $display("result is %d", result);
       end
     endtask
       
